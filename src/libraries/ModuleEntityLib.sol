a82ab2903da1fb306646a838187f977f3d87eea5
Jay Paik <jay@alchemy.com>
2024-09-04 14:48:37 -0400

    feat: use interfaces from reference implementation and bump version

---
+++ src/libraries/ModuleEntityLib.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+//
+// See LICENSE-MIT file for more information
+
+pragma solidity ^0.8.20;
+
+import {ModuleEntity} from "@erc6900/reference-implementation/interfaces/IModularAccount.sol";
+
+library ModuleEntityLib {
+    // Magic value for hooks that should always revert.
+    ModuleEntity internal constant _PRE_HOOK_ALWAYS_DENY = ModuleEntity.wrap(bytes24(uint192(2)));
+
+    function pack(address addr, uint32 entityId) internal pure returns (ModuleEntity) {
+        return ModuleEntity.wrap(bytes24(bytes20(addr)) | bytes24(uint192(entityId)));
+    }
+
+    function unpack(ModuleEntity fr) internal pure returns (address addr, uint32 entityId) {
+        bytes24 underlying = ModuleEntity.unwrap(fr);
+        addr = address(bytes20(underlying));
+        entityId = uint32(bytes4(underlying << 160));
+    }
+
+    function isEmpty(ModuleEntity fr) internal pure returns (bool) {
+        return ModuleEntity.unwrap(fr) == bytes24(0);
+    }
+
+    function notEmpty(ModuleEntity fr) internal pure returns (bool) {
+        return ModuleEntity.unwrap(fr) != bytes24(0);
+    }
+
+    function eq(ModuleEntity a, ModuleEntity b) internal pure returns (bool) {
+        return ModuleEntity.unwrap(a) == ModuleEntity.unwrap(b);
+    }
+
+    function notEq(ModuleEntity a, ModuleEntity b) internal pure returns (bool) {
+        return ModuleEntity.unwrap(a) != ModuleEntity.unwrap(b);
+    }
+}
